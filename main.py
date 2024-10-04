3138eeed89e51e84f74f97189230cb78885647bd
usmaan786 <usmaanchohan78@gmail.com>
2024-10-04 21:35:48 +0100

    Added player sprite and movement/rotation controls

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ def main():
 	print("Screen width:", SCREEN_WIDTH)
 	print("Screen height:", SCREEN_HEIGHT)
 
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
 	clock = pygame.time.Clock()
 	dt = 0
-	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)	
-
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
 		screen.fill((0,0,0))
-		player.draw(screen)
+		
+		for obj in updatable:
+			obj.update(dt)
+
+		for obj in drawable:
+			obj.draw(screen)
+
 		pygame.display.flip()
 		dt = clock.tick(60)/1000
 
